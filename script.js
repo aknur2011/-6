0ad982714c9fa94933b5f87adf7dd3c01188b95f
aknur2011 <aknur8083@gmail.com>
2025-10-29 20:30:32 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let username,password = prompt('login')
+switch (true) {
+    case (username>=12345):
+        alert('кош келиниз админ') 
+        break;
+    case (password>=1234):
+        alert('салам конок')
+         break;
+    default:
+        alert('tyra emes jazсаламdun')
+        break;
+}
+  
+
+
+
